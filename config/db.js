b05ecb9fccc74049d7e3a9e9725239447ee640a4
saurabh solanki <solankisourabh25@gmail.com>
2023-01-19 17:07:01 +0530

    upda

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock");
+  return mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock?retryWrites=true&w=majority");
 };
 
 module.exports = connect;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { default: mongoose } = require('mongoose')
+mongoose.set("strictQuery", true);
 const app = express()
 const signupRoute = require('./routes/signup.routes')
 const loginRoute = require('./routes/login.route')
@@ -17,6 +18,6 @@ app.use("/login",loginRoute)
 app.get('/', (req, res) => res.send('hello'))
 
 app.listen(8000, async() => {
-  await mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock")
+  await mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock?retryWrites=true&w=majority")
   console.log(`server started on port ${8000}`)
 })
