c1cd9c0c0666627606d8f91d909494f6237e061e
saurabh solanki <solankisourabh25@gmail.com>
2023-01-19 16:57:33 +0530

    up

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const { default: mongoose } = require('mongoose')
 const app = express()
 const signupRoute = require('./routes/signup.routes')
 const loginRoute = require('./routes/login.route')
-const connect = require('./config/db')
 
 
 
@@ -16,11 +16,7 @@ app.use("/login",loginRoute)
 
 app.get('/', (req, res) => res.send('hello'))
 
-app.listen(8000, async () => {
-    await connect();
-    console.log("mongodb connected at 8080");
-    try {
-    } catch (e) {
-      console.log(e);
-    }
-  });
+app.listen(8000, async() => {
+  await mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock")
+  console.log(`server started on port ${8000}`)
+})
