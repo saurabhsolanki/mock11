2ad157863e88fd96c276e767c8daf0dedd073164
saurabh solanki <solankisourabh25@gmail.com>
2023-01-20 21:44:39 +0530

    try

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const loginRoute = require('./routes/login.route')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors()) 
+
+app.get('/', (req, res) => res.send('hello'))
 app.use("/signup",signupRoute)
 app.use("/login",loginRoute)
 
 
-app.get('/', (req, res) => res.send('hello'))
 
 app.listen(8000, async() => {
   await mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock?retryWrites=true&w=majority")
