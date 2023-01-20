55f3c0b110085dca66fc61f66aeb131f35fe5490
saurabh solanki <solankisourabh25@gmail.com>
2023-01-20 21:54:48 +0530

    try

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express()
 const signupRoute = require('./routes/signup.routes')
 const loginRoute = require('./routes/login.route')
 
+require("dotenv").config();
+const PORT = process.env.PORT || 9000;
+const URL = process.env.URL || "";
 
 
 app.use(express.urlencoded({extended: true}))
@@ -17,8 +20,7 @@ app.use("/signup",signupRoute)
 app.use("/login",loginRoute)
 
 
-
-app.listen(8000, async() => {
-  await mongoose.connect("mongodb+srv://mock:mock@cluster0.bvmggaj.mongodb.net/mock?retryWrites=true&w=majority")
-  console.log(`server started on port ${8000}`)
+app.listen(PORT, async() => {
+  await mongoose.connect(URL)
+  console.log(`server started on port ${PORT}`)
 })
